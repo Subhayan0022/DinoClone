bedb29adebdc66989d83d03aad5edfda020b1e0d
Subhayan0022 <msubhayan529@gmail.com>
2023-10-30 20:02:30 +0530

    added collision detected mechanic and game over event

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
+font = pygame.font.Font('assets/Pixeltype.ttf', 50)
+
 run = True
 SCREEN_WIDTH = 1058
 SCREEN_HEIGHT = 280
@@ -14,9 +16,11 @@ pygame.display.set_caption('DinoGame')
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 bg_surface = pygame.image.load('assets/bg.png').convert()
 
+game_over = font.render('GAME OVER', False, 'Black')
+
 cact_img = pygame.image.load('assets/cactus.png').convert_alpha()
 cact = pygame.transform.scale(cact_img, (40, 60)).convert_alpha()
-cactxpos = 800
+cactxpos = 900
 cactus_hb = cact.get_rect(midbottom = (cactxpos,225))
 
 
@@ -30,10 +34,10 @@ while(run):
     
     screen.blit(bg_surface,(0, 0))
 
-    cactus_hb.left -= 5
+    cactus_hb.x -= 5
+    if (cactus_hb.right <= 0):
+        cactus_hb.left = 1058
     screen.blit(cact, cactus_hb)
-    if(cactus_hb.left<=-30):
-        cactus_hb.left = 1020
 
     screen.blit(player,player_hb)
 
@@ -47,6 +51,10 @@ while(run):
         if event.type == pygame.QUIT:
             run = False
             sys.exit()
+
+    if(player_hb.colliderect(cactus_hb) == 1):
+        cactus_hb.left = player_hb.right
+        screen.blit(game_over, (450, 50))
 
     pygame.display.update()
     clock.tick(60)
